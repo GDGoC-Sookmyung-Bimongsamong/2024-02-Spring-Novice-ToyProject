bc123c4e2f7d23bad15dbfc34daca70b4207c7c8
minzero31 <77minyoung77@gmail.com>
2024-12-05 02:18:28 +0900

    Feat : 사용자 게시글 조회 목록 PostsRepository에 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts, Long> {
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
+
+    // 사용자 게시글 조회를 목록으로 보여주기 위해 추가
+    List<Posts> findByAuthorIdOrderByCreatedDateDesc(Long authorId);
 }
